3f7294d0159f62795e0869e212c79562ae7e1320
tuananh <nguyentuananhfedev@gmail.com>
2024-02-01 19:42:55 +0700

    first commit

---
+++ src/UserForm.test.js
@@ -0,0 +1,10 @@
+import { screen, render } from "@testing-library/react";
+import user from '@testing-library/user-event';
+import UserForm from './UserForm';
+
+test('Render 2 buttons', () => {
+    // render component 
+
+    // manipulate the components or find components on it
+    // ASSERTION - MAKE SURE
+})
